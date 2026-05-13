b663d8239cddcc1440282c2ff3ceb9f89cf45f67
Kein <kein-of@yandex.ru>
2026-05-13 15:13:34 +0300

    qol updates to pack_mod.bat

--- YourProjectFolder/pack_mod.bat
+++ YourProjectFolder/pack_mod.bat
@@ -1,20 +1,35 @@
 @echo off
 setlocal
+SET r=[31m
+SET w=[37m
 set UATPath=..\Engine\Binaries\DotNET\AutomationTool\AutomationTool.exe
 set ProjectFile=
 set ProjectDir=%~dp0
 set OutputParam=-CopyToGameDir -GameDir=%~2
+
+
+IF NOT EXIST %UATPath% (
+    echo %r%Suppled path is not a valid path to to AutomationTool.exe:%w%
+    echo %UATPath%
+    Exit /B
+)
 
 IF "%~1"=="" (
     echo Mod name is required!
     Exit /B
 )
 
-IF "%~2"=="" (set OutputParam=)
+IF "%~2"=="" (
+    set OutputParam=
+    echo Game folder not specified, packing into Saved/ArchivedPlugins
+)
 
-FOR %%F IN (*.uproject) DO (set ProjectFile=%%F)
-IF "%ProjectFile%"=="" (
-    echo Failed to find uproject file!
+IF "%ProjectFile%"=="" (FOR %%F IN (*.uproject) DO (set ProjectFile=%%F))
+
+
+IF NOT EXIST %ProjectFile% (
+    echo %r%.uproject file not found or path invalid:%w%
+    echo %ProjectFile%
     Exit /B
 )
 
